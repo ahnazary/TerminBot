caa01c1f86f89b2ba6fa3094c8b1aa452b8603aa
ahnazary <ah.nazary.aghchemazary@gmail.com>
2022-12-26 16:09:53 +0100

    Checks if appointments are availabe or not

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from src.selenium import BotInterface
 
 bot = BotInterface()
+
+bot.get_to_third_step()
--- src/selenium.py
+++ src/selenium.py
@@ -1,21 +1,45 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import os
-import time
-import random
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 class BotInterface:
     def __init__(self):
-        driverPath = os.path.abspath(os.path.dirname(__file__)) + '/chromedriver_linux64/chromedriver'
-        self.driver = webdriver.Chrome(driverPath)
-        self.driver.get('https://termine.staedteregion-aachen.de/auslaenderamt/?rs')
-
-
-        button = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="buttonfunktionseinheit-1"]'))
+        driverPath = (
+            os.path.abspath(os.path.dirname(__file__)) + "/chromedriver_linux64/chromedriver"
         )
-        button.click()
-        time.sleep(random.uniform(2, 3))
+        self.driver = webdriver.Chrome(driverPath)
+        self.driver.get("https://termine.staedteregion-aachen.de/auslaenderamt/?rs")
+
+    def get_to_third_step(self):
+        self.press_button('//*[@id="buttonfunktionseinheit-1"]')
+
+        # click on the superC drop down menu
+        self.press_button('//*[@id="header_concerns_accordion-114"]')
+
+        # reserve one time
+        self.press_button('//*[@id="button-plus-191"]')
+
+        # press weiter button
+        self.press_button('//*[@id="WeiterButton"]')
+
+        # press ok button
+        self.press_button('//*[@id="OKButton"]')
+
+        # check if "Kein freier Termin verfügbar" text is present
+        if "Kein freier Termin verfügbar" in self.driver.page_source:
+            print("No free appointment available")
+        else:
+            print("Free appointment available")
+
+        self.driver.close()
+
+    def press_button(self, XPATH, wait_time: int = 5):
+        button = WebDriverWait(self.driver, wait_time).until(
+            EC.element_to_be_clickable((By.XPATH, XPATH))
+        )
+        self.driver.execute_script("arguments[0].click()", button)
