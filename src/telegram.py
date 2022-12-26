cd9b261a936a7f623c6931604d1bf1f62035f1e8
ahnazary <ah.nazary.aghchemazary@gmail.com>
2022-12-26 17:15:58 +0100

    Create telegram module

---
+++ src/telegram.py
@@ -0,0 +1,15 @@
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+# Replace YOUR_BOT_TOKEN with the API key for your bot
+updater = Updater(token='bot token', use_context=True)
+
+def startup_function(update, context):
+  # Send a message to the chat
+  context.bot.send_message(chat_id=update.effective_chat.id, text='Hello, World!')
+
+startup_handler = CommandHandler('start', startup_function)
+updater.dispatcher.add_handler(startup_handler)
+
+# Start the bot
+updater.start_polling()
